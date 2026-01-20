206fd45dfe8f5127449348e89d097ee3e26345e7
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-01-20 16:51:55 +0530

    changes in port

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,5 @@ app.use("/api/finalexam", require("./routes/finalTestRoutes"));
 app.use("/api/contact", require("./routes/contactRoutes"));
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
