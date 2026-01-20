b74e7a924f586c27cb61cb5736bd2647a8c8c5fa
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-01-20 18:07:21 +0530

    changes in server

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://quiz-pro-pi.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "https://quiz-pro-azure.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
