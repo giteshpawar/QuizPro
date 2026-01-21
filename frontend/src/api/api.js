4cc8f3bfa4cf371d6f1490e97f9cb5863b8fd7a3
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-01-21 11:40:57 +0530

    changes in send mail

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://quizpro-8zqq.onrender.com/api",
+  baseURL: "https://quizpro-180b.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
