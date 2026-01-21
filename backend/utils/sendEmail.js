9e3151cc56a570580707f429506fe750ca66a32d
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-01-21 12:20:48 +0530

    changes in send mail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, link) => {
   await resend.emails.send({
     from: "MCQ Test App <onboarding@resend.dev>",
-    to: [to],
+    to: [process.env.RECEIVER_EMAIL],
     subject: "Reset Your Password",
     html: `
       <h3>Password Reset</h3>
